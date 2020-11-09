2187f5e86f4253d8cc737d0df444dfcf7ff26fe2
Jason J Cruz <jaycru06@yahoo.com>
2020-11-09 08:05:44 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -47,12 +47,12 @@ function deleteTask(event) {
 function handlePriorityOnClick() {
   createPriorityList();
   const priorityList = document.querySelector(".priority-list");
-  let taskCounter = 0;
+  let taskCounter = 1;
   priorityList.addEventListener("click", function(event) {
     // event.preventDefault();
     // debugger;
     // let priority = document.querySelector(`.priority${priorityCounter+1}`);
-    let task = document.querySelector(`.task${taskCounter+1}`);
+    let task = document.querySelector(`.task${taskCounter}`);
     let priority1 = event.target.children[0];
     let priority2 = event.target.children[1];
     let priority3 = event.target.children[2];
